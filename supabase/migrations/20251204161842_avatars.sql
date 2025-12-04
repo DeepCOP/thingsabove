b0ba1b19919ff214cd6c0c21086d047f39124695
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2025-12-04 19:50:46 +0300

    add avatars RLS policies

---
+++ supabase/migrations/20251204161842_avatars.sql
@@ -0,0 +1,36 @@
+create policy "avatars are publicly accessible"
+on storage.objects
+for select using (bucket_id = 'avatars');
+
+
+create policy "users can upload their own avatar"
+on storage.objects
+for insert
+with check (
+  bucket_id = 'avatars'
+  and auth.uid()::text = (storage.foldername(name))[1]
+);
+
+
+create policy "users can update their own avatar"
+on storage.objects
+for update
+using (
+  auth.uid() = owner
+)
+with check  (
+  bucket_id = 'avatars'
+  and auth.uid()::text = (storage.foldername(name))[1]
+);
+
+create policy "users can delete their own avatar"
+on storage.objects
+for delete
+using (
+  auth.uid() = owner
+)
+with check  (
+  bucket_id = 'avatars'
+  and auth.uid()::text = (storage.foldername(name))[1]
+);
+
---
+++ supabase/migrations/20251204163436_plan_covers.sql
@@ -0,0 +1,44 @@
+create policy "plan covers publicly accessible"
+on storage.objects
+for select
+using (bucket_id = 'plan_covers');
+
+create policy "authors upload cover images"
+on storage.objects
+for insert
+with check (
+  bucket_id = 'plan_covers'
+  and exists (
+    select 1
+    from public.devotional_plans p
+    where p.id::text = (storage.foldername(name))[1]
+    and p.author_id = auth.uid()
+  )
+);
+
+
+create policy "authors delete cover images"
+on storage.objects
+for delete
+using (
+  bucket_id = 'plan_covers'
+  and exists (
+    select 1
+    from public.devotional_plans p
+    where p.id::text = (storage.foldername(name))[1]
+    and p.author_id = auth.uid()
+  )
+);
+
+create policy "authors update cover images"
+on storage.objects
+for update
+using (
+  bucket_id = 'plan_covers'
+  and exists (
+    select 1
+    from public.devotional_plans p
+    where p.id::text = (storage.foldername(name))[1]
+    and p.author_id = auth.uid()
+  )
+);
