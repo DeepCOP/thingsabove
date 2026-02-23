d52cf688eefef3d99fac307b709ce1e6dae2579e
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2026-02-23 20:21:04 +0300

    fix contribution link issue

--- src/screens/PlansScreen.tsx
+++ src/screens/PlansScreen.tsx
@@ -88,11 +88,11 @@ export default function PlansScreen({
 
       <TouchableOpacity
         onPress={onContribute}
-        className="absolute bottom-4 right-4 flex-row items-center gap-1 bg-rose-400 px-4 py-2 rounded-full">
-        <Text className="text-xs font-bold text-gray-800  underline">
+        className="absolute bottom-4 right-4 flex-row items-center gap-1  px-4 py-2 rounded-full">
+        <Text className="text-xs font-bold text-gray-800 dark:text-gray-300  underline">
           Have a plan you&apos;d like to contribute?
         </Text>
-        <Ionicons name="arrow-forward" />
+        <Ionicons name="arrow-forward" color={colorScheme === 'dark' ? '#fff' : '#000'} />
       </TouchableOpacity>
     </View>
   );
